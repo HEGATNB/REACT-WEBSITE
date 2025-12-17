91807d9d887691126d860818e3cf1b530a7dda75
HEGATNB <misharybakov2006@gmail.com>
2025-12-17 23:38:23 +0300

    Исправлена мобильная версия списка технологий

--- src/pages/technologyList.tsx
+++ src/pages/technologyList.tsx
@@ -1,5 +1,4 @@
 import { Link } from 'react-router-dom';
-import { useState, useEffect } from 'react';
 import './TechnologyList.css';
 import useTechnologiesApi from '../components/TechnologiesApi';
 
